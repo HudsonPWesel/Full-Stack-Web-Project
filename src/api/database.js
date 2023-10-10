4047d6a51e434f4aeab6d6ecf9a9083ced9f19eb
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-10-10 12:45:36 -0400

    Setup-Backend

---
+++ src/api/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('LinkedInEducation', 'root', 'password', {
+	dialect: 'mysql',
+	host: 'localhost',
+});
+
+module.exports = sequelize;
---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../api/database');
+
+const User = sequelize.define('user', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+	firstName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+
+	lastName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	imageUrl: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	subheading: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+});
+
+module.exports = User;
