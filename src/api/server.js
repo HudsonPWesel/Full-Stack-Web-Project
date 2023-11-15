4720d13a83b3e52a4e772463ad6211927c60860a
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-11-15 12:43:24 -0500

    Separate server and app

--- src/api/server.js
+++ src/api/server.js
@@ -1,24 +1,9 @@
-const express = require('express');
-const cors = require('cors');
 const sequlize = require('./database');
 const User = require('../models/user');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const AppError = require('../util/appError');
-
 const { createTemplateUsers, clearUsers } = require('../controllers/user.js');
 
-const classmatesRouter = require('./routes/classmates');
-const loginRouter = require('./routes/login');
+const app = require('./app.js');
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-dotenv.config({path: '/Users/wyld7k/Desktop/Coding-Languages/Projects/fullstack-project/Full-Stack-Web-App/src/api/config.env'});
-
-app.use(morgan('dev'));
-// Implicity knows all defined models using sequlize.define()
-// WARNING --DELETING ALL USERS AT THE START OF NODE SERVER SO NEW USERS WILL BE DELETED
 sequlize
 	.sync()
 	.then(res => {
@@ -61,12 +46,3 @@ sequlize
 	});
 
 
-
-// Todo Maybe make createNewClassmate a new route
-app.use('/classmates', classmatesRouter);
-app.use('/', loginRouter);
-
-
-app.all('*', (req,res,next) => {
-    next(new AppError(`Cannot find ${req.originalUrl}`, 404));
- })
