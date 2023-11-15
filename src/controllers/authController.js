077d7c2fc97f16679d11ed2446b2999ecf9e991e
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-11-15 12:36:49 -0500

    Checkpt

---
+++ src/controllers/authController.js
@@ -0,0 +1,7 @@
+const AppError = require('../util/appError');
+exports.login = (req,res, next) => {
+   //const { email,password} = req.body;
+   //if(!email || !password)
+   //    next(new AppError('Please provide a valid email and password', 400));
+   //next();
+}
--- src/controllers/classmatesController.js
+++ src/controllers/classmatesController.js
@@ -2,8 +2,10 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const { createTemplateUsers, clearUsers } = require('../controllers/user.js');
 
-exports.checkID = (req,res,next, val) => {
-    if (val > 3){
+exports.checkID = async (req,res,next, val) => {
+    const users = await User.findAll();
+    console.log(users);
+    if (val > users.length){
         return res.status(404).json({
             status: 'fail',
             message: 'Invalid ID'
@@ -16,23 +18,31 @@ exports.getClassmate = async (req, res) => {
     const user = users.find(element => 
         element.id == req.params.id
     );
-    
-    res.send(user);
+    res.status(201).json({
+        status: 'success',
+        data : {
+            user
+        }
+    }
+    );
 
 }
 
 exports.getClassmates = async (req, res) => {
     const users = await User.findAll();
-    res.send(users);
+
+    res.status(201).json({
+        status: 'success',
+        data : {
+            users
+        }
+    }
+    );
 
 }
 
 exports.createClassmate = async (req, res) => {
     const data = req.body;
-    console.log(typeof process.env.JWT_EXPIRES);
-    console.log(typeof process.env.JWT_SECRET);
-    console.log(data.id);
-
     const token = jwt.sign( {data: data.id}, process.env.JWT_SECRET, {expiresIn : process.env.JWT_EXPIRES});
 
     createTemplateUsers(data.firstName,data.lastName,data.email,data.imageUrl,data.subheading, data.password, data.passwordConfirm);
